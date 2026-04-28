d841a6a6f0491c629900e3f688639daedc27c225
KsuMiticheva <ksenia.miticheva@gmail.com>
2026-04-28 23:37:29 +0300

    Moved application-test.properties to resources; Added ApiEndpoints class; Added new methods to APIClient (ping, get list of bookings, get booking by id); Added models (Booking, SingleBooking), Added tests (HealthCheck, GetBookings to get list of booking Ids and one booking by id) Amended build.gradle

--- src/main/java/core/clients/APIClient.java
+++ src/main/java/core/clients/APIClient.java
@@ -1,5 +1,10 @@
 package core.clients;
 
+import core.settings.ApiEndpoints;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -26,4 +31,42 @@ public class APIClient {
         }
         return properties.getProperty("baseUrl");
     }
+
+    private RequestSpecification getRequestSpec() {
+        return RestAssured.given()
+                .baseUri(baseUrl)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json");
+    }
+
+    public Response ping() {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.PING.getPath())
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+    }
+
+    public Response getBooking() {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKING.getPath())
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+    }
+
+    public Response getBookingById(int id) {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKING.getPathById(id))
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+    }
+
 }
---
+++ src/main/java/core/settings/ApiEndpoints.java
@@ -0,0 +1,21 @@
+package core.settings;
+
+public enum ApiEndpoints {
+    PING("/ping"),
+    BOOKING("/booking"),
+    BOOKING_BY_ID("/booking/%d");
+
+    private final String path;
+
+    ApiEndpoints(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getPathById(int id) {
+        return path + "/" + id;
+    }
+}
